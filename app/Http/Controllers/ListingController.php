eec6d2de2ba6a86f5d89b6de0ecea16c9fa63cb0
Anthenny <anthenny1997@hotmail.com>
2023-02-28 13:43:10 +0100

    view files georganiseerd volgens de Laravel conventions

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+
+class ListingController extends Controller
+{
+    // Show all listings
+    public function index() {
+        $listings = Listing::all();
+
+        return view('listings.index', [
+            'heading' => 'Latest Listings',
+            'listings' => $listings
+        ]);
+
+    }
+
+    // Show single listing
+    // ELoquent models have route model binding that handles 404 automaticly.
+    public function show(Listing $listing) {
+        return view('listings.show', [
+            'listing' => $listing
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,10 @@
 <?php
 
-use App\Models\Listing;
-use Illuminate\Http\Request;
+use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 // All listings
-Route::get('/', function () {
-
-    $listings = Listing::all();
-
-    return view('listings', [
-        'heading' => 'Latest Listings',
-        'listings' => $listings
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
 // Single listing
-// ELoquent models have route model binding that handles 404 automaticly.
-Route::get('listings/{listing}', function(Listing $listing) {
-    return view('listing', [
-        'listing' => $listing
-    ]);
-
-});
+Route::get('listings/{listing}', [ListingController::class, 'show']);
