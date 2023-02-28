599ea3f4141ed3e8b238fa0cb00d5f9141e8bcd4
Anthenny <anthenny1997@hotmail.com>
2023-02-28 13:32:13 +0100

    Vieuws toegevoegd en geupdate

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// All listings
 Route::get('/', function () {
-    return view('welcome');
+
+    $listings = Listing::all();
+
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => $listings
+    ]);
+});
+
+// Single listing
+// ELoquent models have route model binding that handles 404 automaticly.
+Route::get('listings/{listing}', function(Listing $listing) {
+    return view('listing', [
+        'listing' => $listing
+    ]);
+
 });
